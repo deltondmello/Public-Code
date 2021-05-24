051a49445d58ad6d5a894312e25be5cfd9002141
unknown <deltondmello555@gmail.com>
2021-05-24 09:22:16 +0100

    edit and added to develop branch

--- Password_Gen.py
+++ Password_Gen.py
@@ -1,5 +1,6 @@
-import string
+limport string
 from random import *
 characters = string.ascii_letters + string.punctuation  + string.digits
 password =  "".join(choice(characters) for x in range(randint(10, 12)))
 print(password)
+print('passwd finished')
